117eb1af4c0af285a9945165b565f57768440e21
Anton Kulakov <anton.kulakov@overwolf.com>
2021-05-07 05:38:06 +0300

    fix gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,3 @@
-import { GameEvents } from './game-events';
 export * from './event-emitter';
 export * from './game-events';
 export * from './game-status';
@@ -6,8 +5,4 @@ export * from './launcher-status';
 export * from './overwolf-plugin';
 export * from './overwolf-window';
 export * as Utils from './utils';
-const gameEvents = new GameEvents(['match_info']);
-gameEvents.on('*', (e) => {
-    console.log(e.path, e.category);
-});
 //# sourceMappingURL=index.js.map
