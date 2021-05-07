d67a2f83dc527555205c89c4c2fbb7a82cfd9162
Anton Kulakov <anton.kulakov@overwolf.com>
2021-05-07 05:34:46 +0300

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { GameEvents, GameEvent } from './game-events';
-
 export * from './event-emitter';
 export * from './game-events';
 export * from './game-status';
@@ -7,9 +5,3 @@ export * from './launcher-status';
 export * from './overwolf-plugin';
 export * from './overwolf-window';
 export * as Utils from './utils';
-
-const gameEvents = new GameEvents(['match_info']);
-
-gameEvents.on('*', (e: GameEvent) => {
-  console.log(e.path, e.category);
-});
