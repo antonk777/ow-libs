2a87ef12260392c2c700f313769b3d2e8bee9db9
antonk777 <45145518+antonk777@users.noreply.github.com>
2021-05-07 06:22:24 +0300

    Delete dist directory

--- dist/binder.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function binder<T extends Record<string, any>>(target: T): T;
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './event-emitter';
-export * from './game-events';
-export * from './game-status';
-export * from './launcher-status';
-export * from './overwolf-plugin';
-export * from './overwolf-window';
-export * as Utils from './utils';
